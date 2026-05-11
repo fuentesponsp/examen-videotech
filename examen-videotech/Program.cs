9276d656a3ebc757785132afc6dba3cec0451310
fuentesponsp <fuentesponsp@gmail.com>
2026-05-11 09:53:23 +0200

    Ejercicio 2: fecha, búsqueda y registro

--- examen-videotech/Program.cs
+++ examen-videotech/Program.cs
@@ -37,3 +37,38 @@
         return $"{titulo} - {director} ({anyo})";
     }
 }
+public class main
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Now.ToShortDateString());
+
+        List<Pelicula> peliculas = new List<Pelicula>();
+        peliculas.Add(new Pelicula("I swear", "Un escocés", 2026, false));
+        peliculas.Add(new Pelicula("Cars", "Un inglés", 2003, true));
+        peliculas.Add(new Pelicula("Shrek", "Un galés", 2000, true));
+
+        foreach (Pelicula p in peliculas)
+        {
+            Console.WriteLine(p.ToString());
+        }
+        foreach (Pelicula p in peliculas)
+        {
+            if (p.ToString().Contains("Nolan"))
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+
+    public static void GuardarPeliculas(List<Pelicula> lista, string ruta)
+    {
+        using (StreamWriter writer = new StreamWriter(ruta))
+        {
+            foreach (Pelicula p in lista)
+            {
+                writer.WriteLine($"{p.getTitulo()};{p.getDirector()};{p.getAnyo()};{p.isDisponible()}");
+            }
+        }
+    }
+}
