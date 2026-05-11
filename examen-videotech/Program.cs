4500661768ea098ca307bf14aeb17b5534efe69a
fuentesponsp <fuentesponsp@gmail.com>
2026-05-11 09:50:57 +0200

    Ejercicio 1: clase Película implementada

--- examen-videotech/Program.cs
+++ examen-videotech/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿public class Pelicula{
+    private string titulo;
+    private string director;
+    private int anyo;
+    private bool disponible;
+
+    public Pelicula(string titulo, string director, int anyo, bool disponible)
+    {
+        this.titulo = titulo;
+        this.director = director;
+        this.anyo = anyo;
+        this.disponible = disponible;
+    }
+
+    public string getTitulo()
+    {
+        return titulo;
+    }
+
+    public string getDirector()
+    {
+        return director;
+    }
+
+    public int getAnyo()
+    {
+        return anyo;
+    }
+
+    public bool isDisponible()
+    {
+        return disponible;
+    }
+
+    public override string ToString()
+    {
+        return $"{titulo} - {director} ({anyo})";
+    }
+}
